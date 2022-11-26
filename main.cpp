8fd1468e52b1a3f52215852e66c50909a2151628
Vitia <berbenets.vd@phystech.edu>
2022-11-26 23:17:55 +0300

    added output original text after sorting

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include "onegin.h"
 
 
-int main()
-{      
+int main() {
+
     strings_inform* onegin_strings;
 
     buffer onegin_text;
 
-    ReadFile("onegin.txt", &onegin_text);
+    ReadFile("onegin111.txt", &onegin_text);
 
     onegin_strings = Constructor(&onegin_text);
+
+    strings_inform* onegin_strings_copy = onegin_strings;
  
-    SortFromBeginning(onegin_strings, &onegin_text);
+    SortFromBeginning(onegin_strings_copy, &onegin_text);
 
-    PrintSortingText(onegin_strings, &onegin_text, "SORT FROM THE BEGINNING OF THE LINES");
+    PrintSortingText(onegin_strings_copy, &onegin_text, "SORT FROM THE BEGINNING OF THE LINES");
 
-    SortFromEnd(onegin_strings, &onegin_text);
+    SortFromEnd(onegin_strings_copy, &onegin_text);
 
-    PrintSortingText(onegin_strings, &onegin_text, "SORT FROM THE END OF THE LINES");
+    PrintSortingText(onegin_strings_copy, &onegin_text, "SORT FROM THE END OF THE LINES");
+
+    PrintSortingText(onegin_strings, &onegin_text, "ORIGINAL TEXT");
 
     Destroyer (onegin_strings, &onegin_text);
 
